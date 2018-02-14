17eb3c0f40ef18f58ed03faad5e7c84a815bbf57
mihkelkukk <mihkel.rate@live.com>
2018-02-14 11:30:28 +0200

    message 1

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: TAK16_Kukk
+ * Date: 14.02.2018
+ * Time: 9:42
+ */
+
+
+
+function test(){
+    $items = [];
+    $firstName = "Mike";
+
+    foreach ($items as $item);
+
+};
+
+function test2(){
+
+
+    echo "hello world";
+
+
+};
+
+test2();
+
+test2();
+
+
+
+
+
+test2();
